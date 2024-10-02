27ea519a16aa6f92f6dd29692958a81d8e55c772
PogrammerLord <notrued@gmail.com>
2024-10-02 14:35:41 +0530

    Third commit

--- Pacman.c
+++ Pacman.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define Height 20 
 #define Width 70
+#define MAX_PASS 20
+#define MAX_USER 30
+struct user
+{
+    char pass[MAX_PASS];
+    char user[MAX_USER];
+    int score;
+};
 int map()
 {
     int i,j;
@@ -34,49 +44,79 @@ void displayMessage()
         printf("%s\n",buffer);
     }
     fclose(fptr);
-    //To display the leaderboard 
-
+    //To display the leaderboard
+    FILE *f1ptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Passwords.txt","r");
+    struct user u1;
+    printf("\nLEADERBOARD:\n");
+    printf("USERNAME\tSCORE\n");
+    while(fread(&u1,sizeof(u1),1,fptr))
+    {
+        printf("%s\n",u1.user);
+    }
 }
-void inputUserPass()
+void SignUp()
 {
-    char Username[255],Password[255];
-    printf("Enter the Username:");
-    l1: scanf("%[^\n]s",Username);
-    if(strlen(Username)>255)
+    FILE *fptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Passwords.txt","a");
+    if(fptr==NULL)
     {
-        printf("Invalid Username, Enter a username of length lesser than 255:\n");
-        goto l1;
+        fputs("Error cannot open the file\n",stderr);
+        exit(1);
     }
-    else if(strlen(Username)<1)
+    struct user u1;
+    char pass[30];
+    printf("\nEnter the username:\n");
+    fgets(u1.user,30,stdin);
+    l1: printf("\nEnter the password:\n");
+    fgets(u1.pass,20,stdin);
+    fwrite(&u1,sizeof(u1),1,fptr);
+    fclose(fptr);
+    printf("\nSuccessful Signup!\n\nWelcome %s\n\nEnjoy the game!\n",u1.user);
+}
+void LogIn()
+{
+    FILE *fptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Passwords.txt","r");
+    if(fptr==NULL)
     {
-        printf("Invalid Username, Enter a Username of length greater than 1:\n");
-        goto l1;
+        fputs("Error cannot open the file\n",stderr);
+        exit(1);
     }
-    //Username
-    FILE *uptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Usernames.txt","a");
-    fprintf("%s\n",Username);
-    fclose(uptr);
-    //Passwords
-    printf("Enter the password");
-    l2: scanf("%[^\n]s",Password);
-    if(strlen(Username)>128)
+    struct user u1;
+    char pass[20],user[30];
+    l1: printf("\nEnter the username:\n");
+    fgets(user,30,stdin);
+    printf("\nEnter the password:\n");
+    fgets(pass,20,stdin);
+    while(fread(&u1,sizeof(u1),1,fptr))
     {
-        printf("Invalid Password, Enter a username of length lesser than 128:\n");
-        goto l2;
+        if(strcmp(pass,u1.pass)!=0 || strcmp(user,u1.user)!=0)
+        {
+            printf("Wrong credentials");
+            fclose(fptr);
+            return;
+        }
     }
-    else if(strlen(Username)<1)
-    {
-        printf("Invalid Password, Enter a Username of length greater than 6:\n");
-        goto l2;
-    }
-    FILE *pptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Password.txt","a");
-    fprintf("%s\n",Password);
-    fclose(pptr);
+    fwrite(&u1,sizeof(user),1,fptr);
+    fclose(fptr);
+    printf("Signed in successfully!\n");
 }
 int main()
 {
-    int no_players,diff;
-    return 0;
-
+    int no_players,diff,neworold;
+    l1: printf("\nEnter '1' if new user & '0' if existing user:\n");
+    scanf("%d",&neworold);
+    getchar();
+    if(neworold == 1)
+    {
+        SignUp();
+    }
+    else if(neworold == 0)
+    {
+        LogIn();
+    }
+    else
+    {
+        printf("Invalid Input, Enter again:\n");
+        goto l1;
+    }
+    return 0;    
 }
-
--- Testcodes.c
+++ Testcodes.c
@@ -1,39 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+struct user
+{
+    char pass[255];
+    char user[255];
+};
 int main()
 {
-    char Username[255],Password[255];
-    printf("Enter the Username:");
-    l1: scanf("%[^\n]s",Username);
-    if(strlen(Username)>255)
+    FILE *fptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Passwords.txt","r");
+    if(fptr==NULL)
     {
-        printf("Invalid Username, Enter a username of length lesser than 255:\n");
-        goto l1;
+        fputs("Error opening the file!",stderr);
     }
-    else if(strlen(Username)<1)
+    struct user u1;
+    char pass[255],user[255];
+    l1: printf("\nEnter the username:\n");
+    fgets(user,30,stdin);
+    printf("\nEnter the password:\n");
+    fgets(pass,20,stdin);
+    while(fread(&u1,sizeof(u1),1,fptr))
     {
-        printf("Invalid Username, Enter a Username of length greater than 1:\n");
-        goto l1;
+        if(strcmp(pass,u1.pass)==0 && strcmp(user,u1.user))
+        {
+            printf("Successful Login\n");
+        }
+        else
+        {
+            printf("Incorrect Username or Password\nPlease enter the correct credentials\n");
+        }
     }
-    //Username
-    FILE *uptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Usernames.txt","a");
-    fprintf(uptr,Username);
-    fclose(uptr);
-    //Passwords
-    printf("Enter the password");
-    l2: scanf("%[^\n]s",Password);
-    if(strlen(Username)>128)
-    {
-        printf("Invalid Password, Enter a username of length lesser than 128:\n");
-        goto l2;
-    }
-    else if(strlen(Username)<1)
-    {
-        printf("Invalid Password, Enter a Username of length greater than 6:\n");
-        goto l2;
-    }
-    FILE *pptr = fopen("C:\\1st-Year-Pacaman-C-Package\\Password.txt","a");
-    fprintf(pptr,Password);
-    fclose(pptr);
     return 0;
 }
